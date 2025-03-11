3b51e5e5bce3a5ccaa9c25ba9a70d4ce7490c379
Sammy <sammy.eid27@flhsemail.org>
2025-03-11 13:52:10 -0700

    Initial Commit

---
+++ Swift Icon/Swift_IconApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_IconApp.swift
+//  Swift Icon
+//
+//  Created by Sammy Eid on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_IconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift IconTests/Swift_IconTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_IconTests.swift
+//  Swift IconTests
+//
+//  Created by Sammy Eid on 3/11/25.
+//
+
+import Testing
+@testable import Swift_Icon
+
+struct Swift_IconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
